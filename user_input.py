8eea8d1ac07cec122590ee413a2fd641b42b7ae5
Остин Джесси <116760268+awstine@users.noreply.github.com>
2024-09-17 11:06:33 -0700

    first assignment

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input('whats your name: ')
+age = input('whats your age: ')
+location = input('where do you come from: ')
+
+print("Users name is " +name ,"and is " +age, "years old.He comes from " +location )
