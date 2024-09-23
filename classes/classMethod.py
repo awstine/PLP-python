736260122aca4717aed8c4c8ce2e31e496470be6
Остин Джесси <116760268+awstine@users.noreply.github.com>
2024-09-23 02:45:35 -0700

    OOP exercises

--- classes/classMethod.py
+++ classes/classMethod.py
@@ -7,11 +7,11 @@ p1.displayInfo()
 
 class Person1:
     def __init__(self,name,age):
-        self.name = name
-        self.age = age
+        self._name = name
+        self._age = age
 
     def displayInfo2(self):
-        print('Person name: ',self.name, 'Age: ', self.age)
+        print('Person name: ',self._name, 'Age: ', self._age)
 
 p2 = Person1("Maiko", 33)
 p2.displayInfo2()
